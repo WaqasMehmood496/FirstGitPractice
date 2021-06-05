fcecaf8f761e7c604389abcb9d1b65a87a824f24
malik waqas <malikwaqas100@hotmail.com>
2021-06-06 00:50:45 +0500

    Test Commit

---
+++ GitPracticeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPracticeProject
+//
+//  Created by Waqas Mehmood on 06/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Some print here")
+    }
+
+
+}
+
